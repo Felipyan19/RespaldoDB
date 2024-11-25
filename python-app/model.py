aae1138506d6dcdbbc839243b67b254389398498
Andres Castaño <felipyan199@gmail.com>
2024-11-25 14:54:25 -0500

    feat: validatuion for sync crud databases. Ac

--- python-app/model.py
+++ python-app/model.py
@@ -2,12 +2,11 @@ import sys
 import time
 import datetime
 import mysql.connector
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 import uuid
 import pandas as pd
 from mysql.connector import Error
 
-# DataFrame global para registrar los errores
 failed_records = pd.DataFrame(columns=["id_Respaldo", "nombre", "numero_identidad", "tipo_identidad", "direccion", "foto", "cargo", "hv", "error"])
 
 def conectar_mysql():
@@ -17,7 +16,8 @@ def conectar_mysql():
             port=3307,
             user="crud_user",
             password="crudpassword",
-            database="crud_db"
+            database="crud_db",
+            connect_timeout=2  
         )
         if conexion.is_connected():
             return conexion
@@ -27,10 +27,11 @@ def conectar_mysql():
 
 def conectar_mongo():
     try:
-        client = MongoClient('mongodb://localhost:27017/')
-        db = client['crud_db']
+        client = MongoClient('mongodb://localhost:27017/', serverSelectionTimeoutMS=2000) 
+        db = client['crud_db'] 
+        client.admin.command('ping')
         return db
-    except Exception as e:
+    except errors.ServerSelectionTimeoutError as e:
         print(f"Error al conectar a MongoDB: {e}")
         return None
 
@@ -71,140 +72,147 @@ def fetch_data():
     df_final = pd.concat([df_mysql, df_mongo]).drop_duplicates().reset_index(drop=True)
     return df_final
 
-def insert_data(record):
+def insert_data(record, database=None):
     global failed_records
     responses = []
 
-    # Generar id_Respaldo único
-    id_respaldo = str(uuid.uuid4())
-    record["id_Respaldo"] = id_respaldo
+    if record["id_Respaldo"] is None or record["id_Respaldo"] == "":
+        id_respaldo = str(uuid.uuid4())
+        record["id_Respaldo"] = id_respaldo
 
-    # Insertar en MySQL
-    mysql_db = conectar_mysql()
-    if mysql_db is None:
-        record_with_error = {**record, "error": "Insertar - MySQL"}
-        failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
-        responses.append("No se pudo conectar a MySQL")
-    else:
-        try:
-            cursor = mysql_db.cursor()
-            query = """
-                INSERT INTO empleados (id_Respaldo, nombre, numero_identidad, tipo_identidad, direccion, foto, cargo, hv)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            """
-            values = (
-                record["id_Respaldo"],
-                record["nombre"],
-                record["numero_identidad"],
-                record["tipo_identidad"],
-                record["direccion"],
-                record["foto"],
-                record["cargo"],
-                record["hv"]
-            )
-            cursor.execute(query, values)
-            mysql_db.commit()
-            responses.append("Registro guardado en MySQL")
-        except Exception as e:
-            mysql_db.rollback()
+    if database is None or database == "mysql":
+    
+        mysql_db = conectar_mysql()
+        if mysql_db is None:
             record_with_error = {**record, "error": "Insertar - MySQL"}
             failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
-            responses.append(f"Error en MySQL: {e}")
-        finally:
-            cursor.close()
-            mysql_db.close()
-
-    # Insertar en MongoDB
-    try:
-        db = conectar_mongo()
-        if db is not None:
-            empleados_collection = db['empleados']
-            empleados_collection.insert_one(record)
-            responses.append("Registro guardado en MongoDB")
+            responses.append("No se pudo conectar a MySQL")
         else:
+            try:
+                cursor = mysql_db.cursor()
+                query = """
+                    INSERT INTO empleados (id_Respaldo, nombre, numero_identidad, tipo_identidad, direccion, foto, cargo, hv)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                """
+                values = (
+                    record["id_Respaldo"],
+                    record["nombre"],
+                    record["numero_identidad"],
+                    record["tipo_identidad"],
+                    record["direccion"],
+                    record["foto"],
+                    record["cargo"],
+                    record["hv"]
+                )
+                cursor.execute(query, values)
+                mysql_db.commit()
+                print("Registro guardado en MySQL")
+                responses.append("Registro guardado en MySQL")
+            except Exception as e:
+                mysql_db.rollback()
+
+                record_with_error = {**record, "error": "Insertar - MySQL"}
+                failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
+                responses.append(f"Error en MySQL: {e}")
+            finally:
+                cursor.close()
+                mysql_db.close()
+
+    if database is None or database == "mongodb":
+        
+        try:
+            db = conectar_mongo()
+            if db is not None:
+                empleados_collection = db['empleados']
+                empleados_collection.insert_one(record)
+                print("Registro guardado en MongoDB")
+                responses.append("Registro guardado en MongoDB")
+            else:
+                record_with_error = {**record, "error": "Insertar - MongoDB"}
+                failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
+                responses.append("Error en MongoDB: No se pudo conectar a MongoDB")
+        except Exception as e:
             record_with_error = {**record, "error": "Insertar - MongoDB"}
             failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
-            responses.append("Error en MongoDB: No se pudo conectar a MongoDB")
-    except Exception as e:
-        record_with_error = {**record, "error": "Insertar - MongoDB"}
-        failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
 
-        responses.append(f"Error en MongoDB: {e}")
+            responses.append(f"Error en MongoDB: {e}")
 
     return responses
 
-def update_data(record):
-    global failed_records  # Asegúrate de declarar global antes de usar la variable
+def update_data(record, database=None):
+    global failed_records  
     responses = []
 
-    try:
-        mysql_db = conectar_mysql()
-        if mysql_db is not None:
-            cursor = mysql_db.cursor()
-            query = """
-                UPDATE empleados
-                SET id_Respaldo = %s, nombre = %s, tipo_identidad = %s, direccion = %s, foto = %s, cargo = %s, hv = %s
-                WHERE numero_identidad = %s
-            """
-            values = (
-                record["id_Respaldo"],
-                record["nombre"],
-                record["tipo_identidad"],
-                record["direccion"],
-                record["foto"],
-                record["cargo"],
-                record["hv"],
-                record["numero_identidad"]
-            )
-            cursor.execute(query, values)
-            mysql_db.commit()
-            responses.append("Registro actualizado en MySQL")
-        else:
+    if database is None or database == "mysql":
+        try:
+            mysql_db = conectar_mysql()
+            if mysql_db is not None:
+                cursor = mysql_db.cursor()
+                query = """
+                    UPDATE empleados
+                    SET id_Respaldo = %s, nombre = %s, tipo_identidad = %s, direccion = %s, foto = %s, cargo = %s, hv = %s
+                    WHERE numero_identidad = %s
+                """
+                values = (
+                    record["id_Respaldo"],
+                    record["nombre"],
+                    record["tipo_identidad"],
+                    record["direccion"],
+                    record["foto"],
+                    record["cargo"],
+                    record["hv"],
+                    record["numero_identidad"]
+                )
+                cursor.execute(query, values)
+                mysql_db.commit()
+                print("Registro actualizado en MySQL")
+                responses.append("Registro actualizado en MySQL")
+            else:
+                record_with_error = {**record, "error": "Actualizar - MySQL"}
+                failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
+                responses.append("Error al conectar a MySQL para actualizar.")
+        except Exception as e:
+            if mysql_db:
+                mysql_db.rollback()
             record_with_error = {**record, "error": "Actualizar - MySQL"}
             failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
-            responses.append("Error al conectar a MySQL para actualizar.")
-    except Exception as e:
-        if mysql_db:
-            mysql_db.rollback()
-        record_with_error = {**record, "error": "Actualizar - MySQL"}
-        failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
-        responses.append(f"Error en MySQL: {e}")
-    finally:
-        if mysql_db:
-            cursor.close()
-            mysql_db.close()
-
-    # Actualizar en MongoDB
-    try:
-        db = conectar_mongo()
-        if db is not None:
-            empleados = db['empleados']
-            empleados.update_one(
-                {"id_Respaldo": record["id_Respaldo"]},
-                {"$set": record}
-            )
-            responses.append("Registro actualizado en MongoDB")
-        else:
+            responses.append(f"Error en MySQL: {e}")
+        finally:
+            if mysql_db:
+                cursor.close()
+                mysql_db.close()
+    if database is None or database == "mongodb":
+        try:
+            db = conectar_mongo()
+            if db is not None:
+                empleados = db['empleados']
+                empleados.update_one(
+                    {"id_Respaldo": record["id_Respaldo"]},
+                    {"$set": record}
+                )
+                print("Registro actualizado en MongoDB")
+                responses.append("Registro actualizado en MongoDB")
+            else:
+                record_with_error = {**record, "error": "Actualizar - MongoDB"}
+                failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
+                responses.append("Error al conectar a MongoDB para actualizar.")
+        except Exception as e:
             record_with_error = {**record, "error": "Actualizar - MongoDB"}
             failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
-            responses.append("Error al conectar a MongoDB para actualizar.")
-    except Exception as e:
-        record_with_error = {**record, "error": "Actualizar - MongoDB"}
-        failed_records = pd.concat([failed_records, pd.DataFrame([record_with_error])], ignore_index=True)
-        responses.append(f"Error en MongoDB: {e}")
+            responses.append(f"Error en MongoDB: {e}")
 
-    title = "Actualización Completa" if all("actualizado" in msg for msg in responses) else "Error en Actualización"
-    message = "; ".join(responses)
+        title = "Actualización Completa" if all("actualizado" in msg for msg in responses) else "Error en Actualización"
+        message = "; ".join(responses)
     
     return title, message
 
-def procesar_registros_mysql(failed_records):
-    # Filtrar registros fallidos relacionados con MySQL
+def procesar_registros_mysql():
+    global failed_records
+
     resultado_mysql = failed_records[failed_records['error'].str.contains("MySQL", na=False)]
     if not resultado_mysql.empty:
         print("Procesando registros fallidos en MySQL...")
 
-        # Conectar a MySQL
         mysql_db = conectar_mysql()
         if mysql_db is None:
             print("No se pudo conectar a MySQL.")
@@ -223,29 +231,27 @@ def procesar_registros_mysql(failed_records):
                 "hv": row['hv']
             }
 
-            # Insertar o actualizar según el tipo de error
             if row['error'] == "Insertar - MySQL":
-                insert_data(record)
+                print("Insertando registro en MySQL...")
+                insert_data(record, "mysql")
             elif row['error'] == "Actualizar - MySQL":
-                update_data(record)
+                print("Actualizando registro en MySQL...")
+                update_data(record, "mysql")
         
-        failed_records = failed_records.drop(index=index)
+        failed_records = failed_records[~failed_records['error'].str.contains("MySQL", na=False)]
 
-        
+def procesar_registros_mongo():
+    global failed_records
 
-def procesar_registros_mongo(failed_records):
-    # Filtrar registros fallidos relacionados con MongoDB
     resultado_mongo = failed_records[failed_records['error'].str.contains("MongoDB", na=False)]
     if not resultado_mongo.empty:
         print("Procesando registros fallidos en MongoDB...")
 
-        # Conectar a MongoDB
         db = conectar_mongo()
         if db is None:
             print("No se pudo conectar a MongoDB.")
             return
-        db.close()
-
+        
         for index, row in resultado_mongo.iterrows():
             id_respaldo = row['id_Respaldo']
             record = {
@@ -259,22 +265,24 @@ def procesar_registros_mongo(failed_records):
                 "hv": row['hv']
             }   
 
-            # Insertar o actualizar según el tipo de error
             if row['error'] == "Insertar - MongoDB":
-                insert_data(record)
+                print("Insertando registro en MongoDB...")
+                insert_data(record, "mongodb")
             elif row['error'] == "Actualizar - MongoDB":
-                update_data(record)
+                print("Actualizando registro en MongoDB...")
+                update_data(record, "mongodb")
+        
+        failed_records = failed_records[~failed_records['error'].str.contains("MongoDB", na=False)]
     
 def sync_data():
     start_time = datetime.datetime.now()
-    
     while True:
         print(f"Ejecutando tarea de base de datos...{datetime.datetime.now()}, {datetime.datetime.now() - start_time} segundos transcurridos")
         print(f"{failed_records}")
 
         if not failed_records.empty:
-            procesar_registros_mysql(failed_records)
-            procesar_registros_mongo(failed_records)
+            procesar_registros_mysql()
+            procesar_registros_mongo()
         
         time.sleep(60)
 
